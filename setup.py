203c5bf0e681889c3ea125cd57fd46dac4c03534
oleksis <oleksis.fraga@gmail.com>
2020-07-10 12:38:55 -0400

    Add PictaPlaylist extractor

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ import os.path
 import warnings
 import sys
 
-
 try:
     from setuptools import setup, Command
 
@@ -18,115 +17,114 @@ except ImportError:
     setuptools_available = False
 from distutils.spawn import spawn
 
-
 # Get the version from picta_dl/version.py without importing the package
 exec(compile(open("picta_dl/version.py").read(), "picta_dl/version.py", "exec"))
 
 DESCRIPTION = "Picta video downloader"
-LONG_DESCRIPTION = "Command-line program to download videos from Picta.cu Plataforma de Contenidos Audiovisuales and YouTube.com"
-
+LONG_DESCRIPTION = ("Command-line program to download videos from Picta.cu "
+                    "Plataforma de Contenidos Audiovisuales and YouTube.com")
 
 try:
     # This will create an exe that needs Microsoft Visual C++ 2008
     # Redistributable Package
-    import PyInstaller
-    if PyInstaller.compat.is_win:
+    from PyInstaller import compat as pyi_compat
+
+    if pyi_compat.is_win:
+        # noinspection PyUnresolvedReferences
         from PyInstaller.utils.win32.versioninfo import (
-            VarStruct,
-            VarFileInfo,
-            StringStruct,
-            StringTable,
-            StringFileInfo,
-            FixedFileInfo,
-            VSVersionInfo,
-            SetVersion,
+            VarStruct, VarFileInfo, StringStruct, StringTable,
+            StringFileInfo, FixedFileInfo, VSVersionInfo, SetVersion,
         )
-
-
-    def version2tuple(commit=0):
-        version_list = str(__version__).split(".")
-        if len(version_list) > 3:
-            _commit = int(version_list[3])
-            del version_list[3]
-        else:
-            _commit = commit
-
-        _year, _month, _day = [int(value) for value in version_list]
-        return (_year, _month, _day, _commit)
-
-    def version2str(commit=0):
-        version_tuple = version2tuple(commit)
-        return "%s.%s.%s.%s" % version_tuple
-
-    class build_pyinstaller_exe(Command):
-
-        description = "Build the executable"
-        user_options = []
-        version_file = None
-        if PyInstaller.compat.is_win:
-            version_file = VSVersionInfo(
-                ffi=FixedFileInfo(
-                    filevers=version2tuple(),
-                    prodvers=version2tuple(),
-                    mask=0x3F,
-                    flags=0x0,
-                    OS=0x4,
-                    fileType=0x1,
-                    subtype=0x0,
-                    date=(0, 0),
-                ),
-                kids=[
-                    VarFileInfo([VarStruct("Translation", [0, 1200])]),
-                    StringFileInfo(
-                        [
-                            StringTable(
-                                "000004b0",
-                                [
-                                    StringStruct("CompanyName", "oleksis.fraga@gmail.com"),
-                                    StringStruct("FileDescription", DESCRIPTION),
-                                    StringStruct("FileVersion", version2str()),
-                                    StringStruct("InternalName", "picta-dl.exe"),
-                                    StringStruct(
-                                        "LegalCopyright",
-                                        "https://github.com/oleksis/youtube-dl/tree/picta-dl/LICENSE",
-                                    ),
-                                    StringStruct("OriginalFilename", "picta-dl.exe"),
-                                    StringStruct("ProductName", "Picta-DL"),
-                                    StringStruct("ProductVersion", version2str()),
-                                ],
-                            )
-                        ]
-                    ),
-                ],
-            )
-
-        def initialize_options(self):
-            pass
-
-        def finalize_options(self):
-            pass
-
-        def run(self, version=version_file):
-            spawn(
-                [
-                    "pyinstaller",
-                    "-c",
-                    "-F",
-                    "--icon=assets/picta-dl.ico",
-					"--exclude-module=test",
-                    "--name=picta-dl",
-                    "picta_dl/__main__.py",
-                ],
-                dry_run=self.dry_run,
-            )
-            if version:
-                SetVersion("./dist/picta-dl.exe", version)
-
 except ImportError:
+    pyi_compat = None
     if len(sys.argv) >= 2 and sys.argv[1] == "pyinstaller":
         print("Cannot import pyinstaller", file=sys.stderr)
         exit(1)
 
+
+def version2tuple(commit=0):
+    version_list = str(__version__).split(".")
+    if len(version_list) > 3:
+        _commit = int(version_list[3])
+        del version_list[3]
+    else:
+        _commit = commit
+
+    _year, _month, _day = [int(value) for value in version_list]
+    return _year, _month, _day, _commit
+
+
+def version2str(commit=0):
+    version_tuple = version2tuple(commit)
+    return "%s.%s.%s.%s" % version_tuple
+
+
+class build_pyinstaller_exe(Command):
+
+    description = "Build the executable"
+    user_options = []
+    version_file = None
+    if pyi_compat and pyi_compat.is_win:
+        version_file = VSVersionInfo(
+            ffi=FixedFileInfo(
+                filevers=version2tuple(),
+                prodvers=version2tuple(),
+                mask=0x3F,
+                flags=0x0,
+                OS=0x4,
+                fileType=0x1,
+                subtype=0x0,
+                date=(0, 0),
+            ),
+            kids=[
+                VarFileInfo([VarStruct("Translation", [0, 1200])]),
+                StringFileInfo(
+                    [
+                        StringTable(
+                            "000004b0",
+                            [
+                                StringStruct("CompanyName", "oleksis.fraga@gmail.com"),
+                                StringStruct("FileDescription", DESCRIPTION),
+                                StringStruct("FileVersion", version2str()),
+                                StringStruct("InternalName", "picta-dl.exe"),
+                                StringStruct(
+                                    "LegalCopyright",
+                                    "https://github.com/oleksis/youtube-dl/tree/picta-dl/LICENSE",
+                                ),
+                                StringStruct("OriginalFilename", "picta-dl.exe"),
+                                StringStruct("ProductName", "Picta-DL"),
+                                StringStruct("ProductVersion", version2str()),
+                            ],
+                        )
+                    ]
+                ),
+            ],
+        )
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self, version=version_file):
+        spawn(
+            [
+                "pyinstaller",
+                "-c",
+                "-F",
+                "--icon=assets/picta-dl.ico",
+                "--exclude-module=test",
+                "--name=picta-dl",
+                "picta_dl/__main__.py",
+            ],
+            dry_run=self.dry_run,
+        )
+        if version:
+            SetVersion("./dist/picta-dl.exe", version)
+
+
+pyinstaller_cmd = dict()
 pyinstaller_console = [
     {
         "script": "./picta_dl/__main__.py",
@@ -139,15 +137,12 @@ pyinstaller_console = [
     }
 ]
 
-
 if len(sys.argv) >= 2 and sys.argv[1] == "pyinstaller":
-    make_executalble = True
-    pyinstaller_cmd = {
-        "pyinstaller": build_pyinstaller_exe,
-    }
+    make_executable = True
+    pyinstaller_cmd.update({"pyinstaller": build_pyinstaller_exe})
     params = dict()
 else:
-    make_executalble = False
+    make_executable = False
     files_spec = [
         ("etc/bash_completion.d", ["picta-dl.bash-completion"]),
         ("etc/fish/completions", ["picta-dl.fish"]),
@@ -200,9 +195,8 @@ class build_lazy_extractors(Command):
 
 cmdclass = {"build_lazy_extractors": build_lazy_extractors}
 
-if make_executalble:
+if make_executable:
     cmdclass.update(pyinstaller_cmd)
-
 
 setup(
     name="picta_dl",
